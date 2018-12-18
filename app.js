1ece244024462a8a8a332d08ce3d85749c25ea10
Ron Mason <WHERESRONALD0@GMAIL.COM>
2018-12-18 15:25:12 -0500

    configured for mLab

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRoute = require("./routes/index.js");
 
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(
+    "mongodb://crescent_city_admin:dbadmin1@ds163745.mlab.com:63745/cc_donuts",
+    { useNewUrlParser: true }
+  );
+} else {
+  mongoose.connect(
+    "mongodb://localhost/Cresent_City_Donuts",
+    { useNewUrlParser: true }
+  );
+}
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  "mongodb://localhost/Cresent_City_Donuts",
-  { useNewUrlParser: true }
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -27,6 +34,8 @@ app.use(function(req, res, next) {
 
 app.use("/customer", indexRoute);
 
-app.listen(4000, () => {
-  console.log("Cresent City Server is fired up!");
+app.set("port", process.env.PORT || 4000);
+
+app.listen(app.get("port"), () => {
+  console.log(`PORT: ${app.get("port")}`);
 });
